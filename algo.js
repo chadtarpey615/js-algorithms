88e52082703dae359c3caab3259ac239c2789d3c
Chad Tarpey <chadtarpey@gmail.com>
2022-04-12 19:12:16 -0500

    added a my codewar solution for chalenge

--- algo.js
+++ algo.js
@@ -16,3 +16,46 @@ function likes(names) {
 
     }
 }
+
+// Your task, is to create NxN multiplication table, of size provided in parameter.
+
+// for example, when given size is 3:
+
+
+const multiplicationTable = n => {
+    const result = [];
+    for (let i = 1; i <= n; i++)
+    {
+        const row = [];
+        for (let j = 1; j <= n; j++)
+            row.push(i * j);
+        res.push(row);
+    }
+    return result;
+}
+
+
+const charCount = (str) => {
+    // make an object to return at end
+    let result = {}
+    // loop over string, for each character...
+    for (let i = 0; i < str.length; i++)
+    {
+        let char = str[i].toLowerCase()
+        //    if the char is a number/letter AND is a key in object, add one to count 
+        if (result[char] > 0)
+        {
+            result[char++]
+        }
+        //    if the char is a number/letter AND is a not in object, add it to object and set value 
+        else
+        {
+            result[char] = 1
+        }
+    }
+    return result
+}
+
+
+
+
