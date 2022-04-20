78297d3f6808f5b99a83cf5e3f6f795b70d7fa36
Chad Tarpey <chadtarpey@gmail.com>
2022-04-20 06:23:38 -0500

    created factorail and collectOddValues algos

--- algo.js
+++ algo.js
@@ -270,3 +270,31 @@ const factorial = num => {
     }
     return total
 }
+
+
+const recursiveFactorial = num => {
+    if (num === 1) return 1
+    return num * recursiveFactorial(num - 1)
+}
+
+
+const collectOddValues = arr => {
+    let result = []
+
+    const helper = helperInput => {
+        if (helperInput.length === 0)
+        {
+            return
+        }
+
+        if (helperInput[0] % 2 !== 0)
+        {
+            result.push(helperInput[0])
+        }
+        helper(helperInput.slice(1))
+    }
+
+    helper(arr)
+
+    return result
+}
